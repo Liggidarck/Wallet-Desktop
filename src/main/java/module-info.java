14f25a29c10433d6f78487e953c7432488215d80
george <georgyfilatov@yandex.ru>
2022-06-09 10:08:45 +0300

    init commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module com.george.wallet_desktop {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.george.wallet_desktop to javafx.fxml;
+    exports com.george.wallet_desktop;
+}
